2a39b7abb792305b4bf532585ae0967e64f18679
thiago <rumbleh@gmail.com>
2018-11-21 18:33:38 -0200

    10. Add signup loading state.mp4

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1 class="text-center my-5">Welcome to the home screen</h1>
+</template>
+
+<script>
+    export default {
+        name: "Home.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -3,18 +3,31 @@
         <div class="col-md-6 offset-md-3">
             <div class="card">
                 <div class="card-body my-4">
+
                     <h3 class="text-center">Signup</h3>
                     <div class="form-group">
-                        <input v-model="name" type="text" class="form-control" placeholder="Name">
+                        <input v-bind:class="{ 'is-valid': !errors.name && this.submitted, 'is-invalid': errors.name }" v-model="name" type="text" class="form-control" placeholder="Name">
+                        <div class="errors" v-if="errors.name">
+                            <small class="text-danger" v-for="error in errors.name" :key="error">{{ error }}</small>
+                        </div>
                     </div>
                     <div class="form-group">
-                        <input v-model="email" type="text" class="form-control" placeholder="Email">
+                        <input v-bind:class="{ 'is-valid': !errors.email && this.submitted, 'is-invalid': errors.email }" v-model="email" type="text" class="form-control" placeholder="Email">
+                        <div class="errors" v-if="errors.email">
+                            <small class="text-danger" v-for="error in errors.email" :key="error">{{ error }}</small>
+                        </div>
                     </div>
                     <div class="form-group">
-                        <input v-model="password" type="password" class="form-control" placeholder="Password">
+                        <input v-bind:class="{ 'is-valid': !errors.password && this.submitted, 'is-invalid': errors.password }" v-model="password" type="password" class="form-control" placeholder="Password">
+                        <div class="errors" v-if="errors.password">
+                            <small class="text-danger" v-for="error in errors.password" :key="error">{{ error }}</small>
+                        </div>
                     </div>
                     <div class="form-group text-center">
-                        <button @click="registerUser()" class="btn btn-info form-control">Signup</button>
+                        <button @click="registerUser()" class="btn btn-info form-control" :disabled="loading">
+                            <i class="fas fa-spin fa-spinner" v-if="loading"></i>
+                            {{ loading ? '' : 'Signup' }}
+                        </button>
                     </div>
                 </div>
             </div>
@@ -31,21 +44,29 @@
             return {
                 name: '',
                 email: '',
-                password: ''
+                password: '',
+                errors: {},
+                submitted: false,
+                loading: false
             }
         },
         methods: {
             registerUser(){
+                this.submitted = true
+                this.loading = true
                 Axios.post('https://react-blog-api.bahdcasts.com/api/auth/register', {
                     name: this.name,
                     email: this.email,
                     password: this.password
                 }).then((response) => {
                     const { data } = response.data
-                    localStorage.setItem('auth', JSON.stringfy(data))
+                    localStorage.setItem('auth', JSON.stringify(data))
                     this.$root.auth = data
+                    this.$router.push('home')
                 }).catch(({ response }) => {
-                    console.log(response)
+                    this.errors = response.data
+                }).finally(() => {
+                  this.loading = false
                 })
             }
         }
